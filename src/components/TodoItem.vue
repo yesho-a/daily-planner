d833abcbca60c9735ea0a2bba6ae2f5862fee3d3
Yesho Simon <sepkap123@gmail.com>
2022-05-16 10:18:43 +0300

    added todo item component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>Todo Item</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+};
+</script>
+
+<style scope>
+.todo-list {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
