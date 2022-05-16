f6fc40cd68a9af5cb4db081ee712b2507b9748d2
Yesho Simon <sepkap123@gmail.com>
2022-05-16 11:48:51 +0300

    emit onclick method

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,7 +2,7 @@
   <div class="todo-list" v-bind:class="{'is-complete': todo.completed}">
     <p>
       <input type="checkbox" v-on:change="markComplete" /> {{ todo.title }}
-      <button class="del">x</button>
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
     </p>
   </div>
 </template>
