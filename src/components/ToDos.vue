55127c827798cb37936568fa07b0d5d3c3c344c2
Yesho Simon <sepkap123@gmail.com>
2022-05-16 10:24:19 +0300

    update todo item

--- src/components/ToDos.vue
+++ src/components/ToDos.vue
@@ -2,14 +2,18 @@
   <div>
     <h1>Todos</h1>
     <div v-bind:key="todo.id" v-for="todo in todos">
-      <h3>{{ todo.title }}</h3>
+      <TodoItem />
     </div>
   </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem.vue";
 export default {
   name: "ToDos",
+  components: {
+    TodoItem,
+  },
   props: ["todos"],
 };
 </script>
