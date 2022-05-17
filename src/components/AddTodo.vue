95fdec0683e68b1d4e66202aaab4fc607386e617
Yesho Simon <sepkap123@gmail.com>
2022-05-17 12:57:28 +0300

    add todo method to AddTodo.vue form

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -15,6 +15,23 @@
 <script>
 export default {
   name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        // id:uuid,
+        title: this.title,
+        completed: false,
+      };
+      this.$emit("add-todo", newTodo);
+      this.title = "";
+    },
+  },
 };
 </script>
 
