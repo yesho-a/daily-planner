562c49b93c58edeae7411ff45d4867ee3f66cb44
Yesho Simon <sepkap123@gmail.com>
2022-05-17 12:32:14 +0300

    addtodo form

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <form @submit.prevent="addTodo">
+      <input
+        type="text"
+        v-model="title"
+        name="title"
+        placeholder="Add Todo..."
+      />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
