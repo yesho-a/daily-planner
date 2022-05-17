ee56a89ee280cf2ddfc6a08aff4e1a76e81e0c83
Yesho Simon <sepkap123@gmail.com>
2022-05-17 14:23:48 +0300

    addedrouter

---
+++ src/components/TestX.vue
@@ -0,0 +1,102 @@
+<template>
+  <TodoHeader />
+  <AddTodo v-on:add-todo="addTodo" />
+  <ToDos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld.vue";
+import ToDos from "./components/ToDos.vue";
+import TodoHeader from "./components/layouts/TodoHeader.vue";
+import AddTodo from "./components/AddTodo.vue";
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {
+    ToDos,
+    TodoHeader,
+    AddTodo,
+  },
+  data() {
+    return {
+      todos: [
+        // {
+        //   id: 1,
+        //   title: "Todo One",
+        //   completed: false,
+        // },
+        // {
+        //   id: 2,
+        //   title: "Todo Two",
+        //   completed: true,
+        // },
+        // {
+        //   id: 3,
+        //   title: "Todo Three",
+        //   completed: false,
+        // },
+      ],
+    };
+  },
+  methods: {
+    // deleteTodo(id) {
+    //   this.todos = this.todos.filter((todo) => todo.id !== id);
+    // },
+    deleteTodo(id) {
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(() => (this.todos = this.todos.filter((todo) => todo.id !== id)))
+        .catch((err) => console.log(err));
+    },
+    // addTodo(newTodo) {
+    //   this.todos = [...this.todos, newTodo];
+    // },
+
+    addTodo(newTodo) {
+      const {title, completed} = newTodo;
+
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed,
+        })
+        .then((res) => (this.todos = [...this.todos, res.data]))
+        .catch((err) => console.log(err));
+    },
+  },
+
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => (this.todos = res.data))
+      .catch((err) => console.log(err));
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 2.4d;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+</style>
