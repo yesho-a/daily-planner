e1c2e9d84cb17ee20e7dfb773a9c2730d27fc3d3
Yesho Simon <sepkap123@gmail.com>
2022-05-23 15:41:03 +0300

    delete todo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -42,7 +42,7 @@ export default {
     // },
     deleteTodo(id) {
       axios
-        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .delete(`http://127.0.0.1:8000/api/delete/{id}/${id}`)
         .then(() => (this.todos = this.todos.filter((todo) => todo.id !== id)))
         .catch((err) => console.log(err));
     },
