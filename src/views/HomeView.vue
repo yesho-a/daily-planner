291f31e56cd8f8e20466e27ae0dd1e39cf4b9adc
Yesho Simon <sepkap123@gmail.com>
2022-05-23 15:59:29 +0300

    delete obsolete todos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,23 +17,7 @@ export default {
   },
   data() {
     return {
-      todos: [
-        // {
-        //   id: 1,
-        //   title: "Todo One",
-        //   completed: false,
-        // },
-        // {
-        //   id: 2,
-        //   title: "Todo Two",
-        //   completed: true,
-        // },
-        // {
-        //   id: 3,
-        //   title: "Todo Three",
-        //   completed: false,
-        // },
-      ],
+      todos: [],
     };
   },
   methods: {
